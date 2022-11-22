bb1f617c54c81d2ca384d05a0fe99ba764976ecb
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2022-11-22 16:48:04 +0100

    Moved to bornrule

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="UTF-8") as file:
 
 setup(
     name='stc',
-    version='0.3.1',
+    version='0.3.2',
     packages=['stc'],
     python_requires='>=3',
     install_requires=['numpy', 'pandas', 'sqlalchemy'],
